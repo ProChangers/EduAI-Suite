09e7d3c52498344ea6cd29ffe18d7a6d82ae33a5
Vijay Misal <misalvijay153@gmail.com>
2024-10-06 20:54:14 +0530

    added quizmaster pro which was remained

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,31 +14,31 @@ export const Projects = () => {
     {
       title: "Assignment Aider 📝",
       description: "Unlock the power of AI with Assignment Aider! Just input your assignment questions, and let our advanced language model generate insightful, well-structured answers customized to your handwriting style. Say goodbye to assignment stress and hello to personalized, high-quality responses!",
-     
+
     },
     {
       title: "ClipWise 🎥",
       description: "Elevate your video learning experience with ClipWise! Simply enter any YouTube video link, and receive concise summaries and engaging Q&A sections. Ideal for quick revisions or in-depth understanding, ClipWise helps you grasp key concepts efficiently.",
-     
+
     },
     {
       title: "WebSage 🌐",
       description: "Dive into the web with WebSage! This powerful tool summarizes website content, distilling articles, blogs, and tutorials into digestible insights. With integrated Q&A features, WebSage makes it easy to clarify doubts and enhance your understanding of complex topics.",
-   
+
     },
     {
       title: "DocuNinja 📄",
       description: "Master document review with DocuNinja! Upload any document type—PPTs, DOCs, PDFs, HTMLs, or CSVs—and let our AI generate concise summaries and provide Q&A for further clarification. Extract maximum value from your documents in no time!",
-      
+
     },
     {
       title: "QuickCheat Creator 🥇",
       description: "Prepare for exams with confidence using QuickCheat Creator! Simply enter the topic name to generate customized cheat sheets filled with key concepts and essential information. Perfect for quick reviews and last-minute studying!",
-     
+
     }, {
       title: " QuizMaster Pro 🎉",
       description: "Transform your study sessions with QuizMaster Pro! Input a topic or upload relevant materials, and our intelligent AI will create customized quizzes to reinforce your knowledge. Ideal for self-assessment and collaborative study, QuizMaster Pro makes learning dynamic and enjoyable.",
-      
+
     }
 
   ];
@@ -65,7 +65,7 @@ export const Projects = () => {
                             projects.map((project, index) => {
                               return (
                                 // <BasicExample />
-                                <ProjectCard key={index} title={project.title} description={project.description}  />
+                                <ProjectCard key={index} title={project.title} description={project.description} />
                               )
                             })
                           }
